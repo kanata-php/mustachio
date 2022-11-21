1794658f33e8c1d9e8a9dd9c50e63463614169cd
Savio Resende <savio@savioresende.com.br>
2022-11-20 20:45:05 -0700

    Added helper to replace file line for the app service.

--- src/Service.php
+++ src/Service.php
@@ -30,4 +30,55 @@ class Service
 
         throw new InvalidArgumentException('Invalid engine!');
     }
+
+    /**
+     * Replace File Lines that match the callback condition.
+     * 
+     * This method replace lines at the callables by the string with the same keys.
+     *
+     * @param string $file
+     * @param array $conditions (callable[]) Args: string $lineValue
+     * @param array $values (string[])
+     * @param ?callable $toRemove Callback that returns boolean for wether to remote or not the line.
+     * @return void
+     */
+    public static function replaceFileLineByCondition(
+        string $file,
+        array $conditions,
+        array $values,
+        ?callable $toRemove = null,
+    ) {
+        $tempFile = $file . '.tmp';
+
+        $reading = fopen($file, 'r');
+        $writing = fopen($tempFile, 'w');
+
+        while (!feof($reading)) {
+            $line = fgets($reading);
+
+            if (null !== $toRemove && $toRemove($line)) {
+                continue;
+            }
+
+            $written = false;
+            foreach ($conditions as $key => $condition) {
+                if ($condition($line)) {
+                    $newLine =  $values[$key] . PHP_EOL;
+                    $written = true;
+                }
+            }
+
+            if (!$written) {
+                $newLine = $line;
+            }
+
+            fputs($writing, $newLine);
+        }
+        
+        fclose($reading);
+        fclose($writing);
+
+        unlink($file);
+        rename($tempFile, $file);
+    }
 }
